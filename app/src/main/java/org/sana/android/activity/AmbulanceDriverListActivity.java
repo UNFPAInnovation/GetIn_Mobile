a205a7305c06fcd26db8199d4c9db2a368862f95
Martin Galiwango <marting45@live.com>
2016-12-07 14:02:38 +0300

    implemented the call funcationality

--- app/src/main/java/org/sana/android/activity/AmbulanceDriverListActivity.java
+++ app/src/main/java/org/sana/android/activity/AmbulanceDriverListActivity.java
@@ -3,6 +3,7 @@ package org.sana.android.activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.sana.R;
 import org.sana.android.fragment.AmbulanceDriverListFragment;
@@ -28,6 +30,10 @@ public class AmbulanceDriverListActivity extends FragmentActivity implements Amb
     private Cursor mCursor;
     private CursorAdapter mCursorAdapter;
     private Uri mUri = AmbulanceDrivers.CONTENT_URI;
+    String firstName;
+    String lastName;
+    String fullName;
+    String phoneNumber;
 
     static final String[] mProjection = new String[] {
             AmbulanceDrivers.Contract._ID,
@@ -100,22 +106,52 @@ public class AmbulanceDriverListActivity extends FragmentActivity implements Amb
         }else{
             //handle the results from the cursor
 
-            mCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,
-                    mCursor,new String[]{AmbulanceDrivers.Contract.FIRST_NAME, AmbulanceDrivers.Contract.LAST_NAME,
-            AmbulanceDrivers.Contract.PHONE_NUMBER},new int[]{android.R.id.text1,android.R.id.text2,
-            R.id.phoneNumber});
-            //Toast.makeText(this, "results from the cursor \n", Toast.LENGTH_SHORT).show();
+            int position = mCursor.getPosition();
+
+            /**
+             * implement the simpleCursorAdapter
+             * it displays the phone number only
+             */
+            mCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.select_dialog_item,
+                    mCursor,new String[]{AmbulanceDrivers.Contract.PHONE_NUMBER},new int[]{android.R.id.text1});
+
+            /**
+             * getting the selected item from the simpleCursorAdapter
+             */
+            Cursor cursor = (Cursor)mCursorAdapter.getItem(position);
+            if(cursor.moveToFirst()){
+                do{
+                    //retrieve the phone number, first name and the last name from the cursor
+                    firstName = cursor.getString(cursor.getColumnIndex(AmbulanceDrivers.Contract.FIRST_NAME));
+                    lastName = cursor.getString(cursor.getColumnIndex(AmbulanceDrivers.Contract.LAST_NAME));
+                    phoneNumber = cursor.getString(cursor.getColumnIndex(AmbulanceDrivers.Contract.PHONE_NUMBER));
+
+                    //combine the first name and the last name
+                    fullName = firstName + " " +lastName;
+
+                }while(cursor.moveToNext());
+            }
+            //create an alert dialog to display the ambulance driver, create a call button and a cancel button
             final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Driver details")
+            builder.setTitle(fullName )  //set the title for the dialog to the name of the ambulance driver
                     .setAdapter(mCursorAdapter, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-
+//                            Toast.makeText(AmbulanceDriverListActivity.this, ""+which, Toast.LENGTH_SHORT).show();
+                            //create an intent to call the ambulance driver
+                            Toast.makeText(AmbulanceDriverListActivity.this, ""+phoneNumber, Toast.LENGTH_SHORT).show();
                         }
                     })
                     .setPositiveButton("Call", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+//                            if(phoneNumber == null){
+                                String uri = "tel:"+phoneNumber;
+                                Intent intent = new Intent(Intent.ACTION_CALL);
+                                intent.setData(Uri.parse(uri));
+                                startActivity(intent);
+//                            }else
+//                                Toast.makeText(AmbulanceDriverListActivity.this, "no phone number saved"+phoneNumber, Toast.LENGTH_SHORT).show();
 
                         }
                     })
