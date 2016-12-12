f7389f3a6013aa41f82389f7320e4a641afc97cd
donrukanga1 <donaldrukanga@gmail.com>
2016-12-12 16:33:41 +0300

    Added follow up note for midwife

--- app/src/main/java/org/sana/android/util/SanaUtil.java
+++ app/src/main/java/org/sana/android/util/SanaUtil.java
@@ -360,6 +360,7 @@ public class SanaUtil {
         insertProcedure(ctx, R.raw.mapping_form_midwife);
        // insertProcedure(ctx, R.raw.demonstration);
        // insertProcedure(ctx, R.raw.registration_short_en);
+        insertProcedure(ctx, R.raw.followup_note_midwife);
         insertProcedure(ctx, R.raw.midwife_appointment_note);
         insertProcedure(ctx, R.raw.follow_up_note_vht1);
         //insertProcedure(ctx, R.raw.mapping_form_vht);
