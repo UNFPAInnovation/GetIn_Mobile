6180c76a65a0c27ffda3bb761c0aecb8764c6ede
Eric Winkler <winkler.em@gmail.com>
2016-12-18 21:57:50 -0500

    Partial implementation of follow up visits
    Some sections need to be completed. All of it will need to be tested. Added TODOs where more
    work needs to be done.
    
    This could be moved to a separate component if desired.

--- app/src/main/java/org/sana/android/activity/PatientViewActivity.java
+++ app/src/main/java/org/sana/android/activity/PatientViewActivity.java
@@ -7,9 +7,12 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.util.Pair;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,21 +20,27 @@ import android.widget.TextView;
 import org.sana.R;
 import org.sana.android.content.Intents;
 import org.sana.android.content.Uris;
+import org.sana.android.content.core.EncounterWrapper;
 import org.sana.android.content.core.PatientWrapper;
 import org.sana.android.fragment.PatientListFragment;
 import org.sana.android.procedure.Procedure;
+import org.sana.android.provider.BaseContract;
 import org.sana.android.provider.Encounters;
 import org.sana.android.provider.Observations;
 import org.sana.android.provider.Patients;
 import org.sana.api.IModel;
 import org.sana.api.task.EncounterTask;
 import org.sana.core.Encounter;
+import org.sana.core.Observation;
 import org.sana.core.Observer;
 import org.sana.core.Patient;
+import org.sana.util.DateUtil;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -45,9 +54,10 @@ public class PatientViewActivity extends MainActivity {
     static final SimpleDateFormat sdf = new SimpleDateFormat(IModel.DATE_FORMAT,
             Locale.US);
     static java.text.DateFormat shortDateFormat = null;
+    Patient patient = null;
 
     // public Patient getPatient(Uri uri) {
-      // return (Patient) PatientWrapper.get(this, uri);
+    // return (Patient) PatientWrapper.get(this, uri);
     //}
     public static Uri withAppendedUuid(Uri uri, String uuid) {
 
@@ -68,7 +78,7 @@ public class PatientViewActivity extends MainActivity {
 
         mSubject = getIntent().getData();
         String uuid = mSubject.getLastPathSegment();
-        Patient patient=  getPatient(mSubject);
+        patient = getPatient(mSubject);
 
         age = (TextView) findViewById(R.id.girlDetail_age);
         trimester = (TextView) findViewById(R.id.girlDetail_trimester);
@@ -107,16 +117,17 @@ public class PatientViewActivity extends MainActivity {
         // loadAppointments(mListView, uuid);
     }
 
-    public void setText(int id, String val){
+    public void setText(int id, String val) {
         TextView view = (TextView) findViewById(id);
         view.setText(val);
     }
 
-    public String calculateTrimester(int days){
-        if(days <= 104) {
+    // TODO Check that these values are correct
+    public String calculateTrimester(int days) {
+        if (days <= 104) {
             // First <= 104 days
             return "1st";
-        } else if(days > 104 && days <= 195) {
+        } else if (days > 104 && days <= 195) {
             // Second > 104 and <= 195
             return "2nd";
         } else {
@@ -125,26 +136,66 @@ public class PatientViewActivity extends MainActivity {
 
     }
 
-    public void loadAppointmentNotes(ListView view, String uuid){
+    // TODO finish this - these could be moved to a discrete component
+    public void loadAppointmentNotes(ViewGroup root, String uuid) {
         // query encounter table
-        // selection = Encounters.Contract.SUBJECT = 'uuid'
-        // projection = new String[]{ BaseContract.ID, BaseContract.CREATED };
-        // getContentResolver().query(Encounters.CONTENT_URI, projection, selection, null, null);
+        String selection = Encounters.Contract.SUBJECT + "= '" + patient.getUuid() + "'";
+        Cursor cursor = null;
+        EncounterWrapper wrapper = null;
+        List<Encounter> encounters = new ArrayList<>();
+        try {
+            cursor = getContentResolver().query(Encounters.CONTENT_URI, null,
+                    selection, null, BaseContract.CREATED +" ASC");
+            wrapper = new EncounterWrapper(cursor);
+            while (wrapper.moveToNext()) {
+                encounters.add((Encounter)wrapper.getObject());
+            }
+        } catch (Exception e) {
 
-
-        // For each encounter in query
-            // Load meta data for each encounter to a view
-            // Probably just the date cursor from query column(1)
-
-            // query observation table
-            // selection = Observations.Contract.ENCOUNTER +" = '" +
-            // projection =
-            //getContentResolver().query(Observations.CONTENT_URI,null, selection, null, null);
-
+        } finally {
+            if (wrapper != null) wrapper.close();
+        }
+        cursor = null;
+        // Replace this with ListView etc. that holds each follow up child View
+        LayoutInflater inflater = LayoutInflater.from(this);
+        for (Encounter e: encounters) {
+            // For each encounter in query
+            // Replace 0 with resourceId for follow up note view
+            View child = inflateEncounter(inflater, root, 0,0, e);
+            root.addView(child);
+        }
         // This is going to require downsync to get any appointment
         // notes from other VHTs and midwives
     }
 
+    // TODO
+    public View inflateEncounter(LayoutInflater inflater, ViewGroup root,
+                                 int resId, int subResId, Encounter encounter){
+        // Inflate root view of follow up note child
+        ViewGroup child = null;
+
+        // query observation table by UUID of Encounter
+        // selection = Observations.Contract.ENCOUNTER +" = '" +
+        // Return only columns we need
+        // projection = ???
+        Cursor cursor = null;
+        // cursor = getContentResolver().query(Observations.CONTENT_URI,null, selection, null, null);
+        //
+        // While cursor.moveToNext
+        View notes = inflateObservations(inflater, child, subResId, cursor);
+        // add as child to root - which is the Encounter meta data
+        child.addView(notes);
+        return child;
+    }
+
+    // TODO
+    public View inflateObservations(LayoutInflater inflater, ViewGroup root,
+                                 int resId, Cursor cursor){
+        // Follows same pattern as inflateEncounters
+        // inflate each observation view
+        // return loaded view
+        return null;
+    }
 
     public void submit(View view){
         switch(view.getId()){
@@ -153,6 +204,19 @@ public class PatientViewActivity extends MainActivity {
                 onSaveAppState(intent);
                 setResult(RESULT_OK, intent);
                 finish();
+                break;
+            case R.id.btn_call_pnumber:
+                if(!TextUtils.isEmpty(patient.getpNumber())) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL,
+                            Uri.parse("tel:" + patient.getpNumber()));
+                }
+                break;
+            case R.id.btn_call_powerholder:
+                if(!TextUtils.isEmpty(patient.getHolder_pNumber())) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL,
+                            Uri.parse("tel:" + patient.getHolder_pNumber()));
+                }
+                break;
         }
     }
 
