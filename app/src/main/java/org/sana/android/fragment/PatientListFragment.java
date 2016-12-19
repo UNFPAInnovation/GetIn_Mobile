451b1474981e08e7c366b2261f8fe02a243fdf68
Eric Winkler <winkler.em@gmail.com>
2016-12-19 13:40:10 -0500

    Stubbed out some TODOs
    Also replaced explicit int usage for column getters.

--- app/src/main/java/org/sana/android/fragment/PatientListFragment.java
+++ app/src/main/java/org/sana/android/fragment/PatientListFragment.java
@@ -58,14 +58,16 @@ public class PatientListFragment extends ListFragment implements LoaderCallbacks
 
     private static final int PATIENTS_LOADER = 0;
     static final String[] mProjection = new String[] {
-		Contract._ID, 
-		Contract.GIVEN_NAME, 
-		Contract.FAMILY_NAME, 
-		Contract.PATIENT_ID,
-		Contract.LOCATION,
-		Contract.IMAGE,
+		    Contract._ID,
+		    Contract.GIVEN_NAME,
+		    Contract.FAMILY_NAME,
+            // TODO Remove or replace system_id
+		    Contract.PATIENT_ID,
+		    Contract.VILLAGE,
+		    Contract.IMAGE,
+            // Replace with phone number?
             Contract.EDD,
-        Contract.DOB
+            Contract.DOB
 		};
     
     private Uri mUri;
@@ -310,20 +312,29 @@ public class PatientListFragment extends ListFragment implements LoaderCallbacks
                     image.setImageResource(R.drawable.ic_contact_picture);
             	}
             } 
-            
-            String familyName = ((Cursor) getItem(position)).getString(2);
-            String givenName = ((Cursor) getItem(position)).getString(1);
+            // int Column from mProjection used to get field
+            // Should be the same as what get returned by looking up the
+            // column index using:
+            //      cursor.getColumnIndex(Contract.FAMILY_NAME);
+            // etc.
+            String familyName = ((Cursor) getItem(position)).getString(
+                    cursor.getColumnIndex(Contract.FAMILY_NAME));
+            String givenName = ((Cursor) getItem(position)).getString(
+                    cursor.getColumnIndex(Contract.GIVEN_NAME));
             String displayName = StringUtil.formatPatientDisplayName(givenName, familyName);
             TextView name = (TextView) view.findViewById(R.id.name);
             name.setText(displayName);
 
+            // TODO Replace or remove system_id in the layout
             TextView systemId = (TextView)view.findViewById(R.id.system_id);
-            String id = ((Cursor) this.getItem(position)).getString(3);
+            String id = ((Cursor) this.getItem(position)).getString(
+                    cursor.getColumnIndex(Contract.PATIENT_ID));
             //String id = mWrapper.getStringField(Contract.PATIENT_ID);
             systemId.setText((TextUtils.isEmpty(id)? "000000":id));
 
             TextView dobView = (TextView)view.findViewById(R.id.dob);
-            String dobStr = ((Cursor) this.getItem(position)).getString(6);
+            String dobStr = ((Cursor) this.getItem(position)).getString(
+                    cursor.getColumnIndex(Contract.DOB));
             String localDobStr = null;
             Date dob = null;
             try {
@@ -336,7 +347,8 @@ public class PatientListFragment extends ListFragment implements LoaderCallbacks
                     localDobStr));
 
             TextView eddView = (TextView)view.findViewById(R.id.EDD);
-            String eddStr = ((Cursor) this.getItem(position)).getString(5);
+            String eddStr = ((Cursor) this.getItem(position)).getString(
+                    cursor.getColumnIndex(Contract.EDD));
             String localEddStr = null;
             Date edd1 = null;
             try {
@@ -349,7 +361,8 @@ public class PatientListFragment extends ListFragment implements LoaderCallbacks
                     localEddStr));
             
             TextView location = (TextView)view.findViewById(R.id.location);
-            String locationVal = ((Cursor) this.getItem(position)).getString(4);
+            String locationVal = ((Cursor) this.getItem(position)).getString(
+                    cursor.getColumnIndex(Contract.VILLAGE));
             location.setText(locationVal);
             
             
