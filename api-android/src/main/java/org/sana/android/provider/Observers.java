e8258fdb7b67dd543a2387a99895b79f4c137cef
Eric Winkler <winkler.em@gmail.com>
2016-12-23 17:01:19 -0500

    Update Observer contract to match POJO.

--- api-android/src/main/java/org/sana/android/provider/Observers.java
+++ api-android/src/main/java/org/sana/android/provider/Observers.java
@@ -79,5 +79,11 @@ public class Observers{
 
 		/** Secondary contact information for this person */
 		public static final String CONTACT2 = "contact2";
+
+        public static final String FIRST_NAME = "first_name";
+
+        public static final String LAST_NAME = "last_name";
+
+        public static final String LOCATIONS = "locations";
 	}
 }
