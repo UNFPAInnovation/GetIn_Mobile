572b74499ed3bdf0eba5b78c82e5aa3b015b3cd5
Martin Galiwango <marting45@live.com>
2016-12-27 21:21:02 +0300

    worked on the VHT list

--- api/src/main/java/org/sana/core/VHT.java
+++ api/src/main/java/org/sana/core/VHT.java
@@ -1,5 +1,7 @@
 package org.sana.core;
 
+import com.google.gson.annotations.Expose;
+
 /**
  * Created by Marting on 21/11/2016.
  */
@@ -7,7 +9,44 @@ package org.sana.core;
 public class VHT extends Model {
     public static final String TAG = VHT.class.getSimpleName();
     // Add fields
+    @Expose
     public String phoneNumber;
+    @Expose
     public String firstName;
-    public String surname;
+    @Expose
+    public String lastName;
+    @Expose
+    public String location;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLastname() {
+        return lastName;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
