e3e469743eedfad6fc8db4da3387281f44fc4146
Eric Winkler <winkler.em@gmail.com>
2016-12-27 16:49:58 -0500

    Fix parameterized type

--- api/src/main/java/org/sana/net/http/handler/EncounterResponseHandler.java
+++ api/src/main/java/org/sana/net/http/handler/EncounterResponseHandler.java
@@ -3,16 +3,16 @@ package org.sana.net.http.handler;
 import java.lang.reflect.Type;
 import java.util.Collection;
 
-import org.sana.api.task.EncounterTask;
+import org.sana.core.Encounter;
 import org.sana.net.Response;
 
 import com.google.gson.reflect.TypeToken;
 
-public class EncounterResponseHandler extends ApiResponseHandler<Response<Collection<EncounterTask>>>{
+public class EncounterResponseHandler extends ApiResponseHandler<Response<Collection<Encounter>>>{
 
 	@Override
 	public Type getType() {
-		Type typeOf = new TypeToken<Response<Collection<EncounterTask>>>(){}.getType();
+		Type typeOf = new TypeToken<Response<Collection<Encounter>>>(){}.getType();
 		return typeOf;
 	}
 }
