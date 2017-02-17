ac435a3155445c2ad39a7b9c7b65e53fdc954f91
donrukanga1 <donaldrukanga@gmail.com>
2017-02-17 12:07:39 +0300

    Added age limit functionality for up to 24 years

--- app/src/midwife/java/org/sana/android/activity/MainActivity.java
+++ app/src/midwife/java/org/sana/android/activity/MainActivity.java
@@ -938,6 +938,7 @@ public class MainActivity extends BaseActivity implements AuthenticationDialogLi
         String anc = patient.getANC_status();
         Date dob = patient.getDob();
         Date anc_visit = patient.getANC_visit();
+        int age =patient.getAge();
 
         //String uuid = this.get
         String uuid1 = mSubject.getLastPathSegment();
@@ -968,7 +969,7 @@ public class MainActivity extends BaseActivity implements AuthenticationDialogLi
         long no_LMD = days / (60 * 24 * 60 * 1000);
         Log.i(TAG, "the number of lmd days are" + no_LMD);
 
-        if (no_LMD > 84 && anc.equals("No")&& age < 8760) {
+        if (no_LMD > 84 && anc.equals("No")&& age < 25) {
             Calendar c1 = Calendar.getInstance();
             //c1.setTime(new Date());
 
@@ -1025,7 +1026,7 @@ public class MainActivity extends BaseActivity implements AuthenticationDialogLi
         Get a random number , add it to the day she was mapped
         Then work out the logic of her appointment being between Tuesday and Thursday
          */
-        else if(no_LMD < 84 && anc.equals("No")&& age < 8760){
+        else if(no_LMD < 84 && anc.equals("No")&& age < 25){
             Calendar c = Calendar.getInstance();
             int new_lmd = (int)no_LMD; // cast lmd to int (maximum of random number to be generated)
            int rand_diff = 84-new_lmd; // The minimum of the random number to be generated
@@ -1090,7 +1091,7 @@ public class MainActivity extends BaseActivity implements AuthenticationDialogLi
         /*if girl has ever attended ANC
         *Pick ANC date from card
          */
-        else if (anc.equals("Yes")){
+        else if (anc.equals("Yes")&& age < 25){
             Date due_on = patient.getANC_visit();
 
             task.due_on = due_on;
