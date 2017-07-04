46ef8894bd97afe01e12888867eaab132abd8a68
Eric Winkler <winkler.em@gmail.com>
2017-07-04 13:24:38 -0400

    Add synch helpers and state

--- api-android/src/main/java/org/sana/android/provider/Models.java
+++ api-android/src/main/java/org/sana/android/provider/Models.java
@@ -27,6 +27,7 @@
  */
 package org.sana.android.provider;
 
+import org.sana.android.content.Uris;
 import org.sana.core.Concept;
 import org.sana.core.Encounter;
 import org.sana.core.Event;
@@ -38,7 +39,14 @@ import org.sana.core.Procedure;
 import org.sana.core.Subject;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Sana Development
@@ -63,4 +71,153 @@ public class Models {
 		Procedure.class,
 		Subject.class,
 	};
+
+    public static final Uri[] SYNCHABLE = new Uri[]{
+            Subjects.CONTENT_URI,
+            Encounters.CONTENT_URI,
+            EncounterTasks.CONTENT_URI
+    };
+
+    public interface Synch {
+        int MODIFIED = -1;
+        int UP_TO_DATE = 0;
+        int PENDING = 1;
+        int NEW = 2;
+    }
+
+    public static void markModified(Context context, Collection<Uri> list) {
+        for (Uri uri : list) {
+            markPending(context, uri);
+        }
+    }
+
+    public static void markModified(Context context, Uri uri) {
+        ContentValues values = new ContentValues();
+        markModified(values);
+        context.getContentResolver().update(uri, values, null, null);
+    }
+
+    public static void markModified(ContentValues values) {
+        if (!values.containsKey(BaseContract.SYNCH))
+            values.put(BaseContract.SYNCH, Synch.MODIFIED);
+    }
+
+    public static void markNew(Context context, Uri uri) {
+        ContentValues values = new ContentValues();
+        markNew(values);
+        context.getContentResolver().update(uri, values, null, null);
+    }
+
+    public static void markNew(ContentValues values) {
+        if (!values.containsKey(BaseContract.SYNCH))
+            values.put(BaseContract.SYNCH, Synch.NEW);
+    }
+
+    public static void markUpToDate(Context context, Uri uri) {
+        ContentValues values = new ContentValues();
+        markUpToDate(values);
+        context.getContentResolver().update(uri, values, null, null);
+    }
+
+    public static void markUpToDate(ContentValues values) {
+        if (!values.containsKey(BaseContract.SYNCH))
+            values.put(BaseContract.SYNCH, Synch.UP_TO_DATE);
+    }
+
+    public static void markPending(Context context, Collection<Uri> list) {
+        for (Uri uri : list) {
+            markPending(context, uri);
+        }
+    }
+
+    public static void markPending(Context context, Uri uri) {
+        ContentValues values = new ContentValues();
+        markPending(values);
+        context.getContentResolver().update(uri, values, null, null);
+    }
+
+    public static void markPending(ContentValues values) {
+        if (!values.containsKey(BaseContract.SYNCH))
+            values.put(BaseContract.SYNCH, Synch.PENDING);
+    }
+
+    public static final List<Uri> getReadyToSynch(Context context) {
+        List<Uri> list = new ArrayList<>();
+        for(Uri uri:SYNCHABLE) {
+            list.addAll(Models.getWaitingToSynch(context, uri));
+        }
+        return list;
+    }
+
+    public static final List<Uri> getReadyToSynchNew(Context context) {
+        List<Uri> list = new ArrayList<>();
+        for(Uri uri:SYNCHABLE) {
+            list.addAll(Models.getWaitingToSynchNew(context,uri));
+        }
+        return list;
+    }
+
+    public static final List<Uri> getReadyToSynchPending(Context context) {
+        List<Uri> list = new ArrayList<>();
+        for(Uri uri:SYNCHABLE) {
+            list.addAll(Models.getWaitingToSynchPending(context,uri));
+        }
+        return list;
+    }
+
+
+    public static List<Uri> getWaitingToSynch(Context context, Uri uri, int state) {
+        List<Uri> list = new ArrayList<>();
+        Cursor cursor = null;
+        int descriptor = Uris.getDescriptor(uri);
+
+        String selection = null;
+        String[] projection= null;
+        switch (descriptor) {
+            case Uris.SUBJECT_DIR:
+            case Uris.ENCOUNTER_DIR:
+            case Uris.ENCOUNTER_TASK:
+                selection = BaseContract.SYNCH + " = ?";
+                projection = new String[]{
+                        String.valueOf(state)
+                };
+                break;
+            default:
+                return list;
+        }
+        try {
+            cursor = context.getContentResolver().query(uri,
+                    new String[]{BaseContract.UUID}, selection,
+                    projection, null);
+            while (cursor.moveToNext()) {
+                String uuid = cursor.getString(0);
+                list.add(Uris.withAppendedUuid(uri, uuid));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) cursor.close();
+        }
+        return list;
+    }
+
+    public static List<Uri> getWaitingToSynch(Context context, Uri uri) {
+        List<Uri> list = new ArrayList<>();
+        list.addAll(getWaitingToSynch(context, uri, Models.Synch.MODIFIED));
+        return list;
+    }
+
+
+    public static List<Uri> getWaitingToSynchNew(Context context, Uri uri) {
+        List<Uri> list = new ArrayList<>();
+        list.addAll(getWaitingToSynch(context, uri, Models.Synch.NEW));
+        return list;
+    }
+
+
+    public static List<Uri> getWaitingToSynchPending(Context context, Uri uri) {
+        List<Uri> list = new ArrayList<>();
+        list.addAll(getWaitingToSynch(context, uri, Models.Synch.PENDING));
+        return list;
+    }
 }
